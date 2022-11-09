7f8e03831938d7ca0984312f56bf61af894a9404
csb9427 <csb9427@naver.com>
2022-11-09 16:05:35 +0900

    fix: movie 생성 기능 수정

--- seminar4/src/controller/netflixController.ts
+++ seminar4/src/controller/netflixController.ts
@@ -42,6 +42,25 @@ const createnetflixMovie = async (req: Request, res: Response) => {
   ) {
     return res.status(400).json({ status: 404, message: "영화 생성 실패" });
   }
+
+  const data = await netflixService.createnetflixMovie(
+    title,
+    image,
+    iconImage,
+    runningTime,
+    watchTime,
+    epiNum,
+    currentNum,
+    detail,
+    character,
+    genre,
+    seriesDescription
+  );
+
+  if (!data) {
+    return res.status(400).json({ status: 404, message: "영화 생성 실패" });
+  }
+  return res.status(200).json({ status: 200, message: "영화 생성 성공", data });
 };
 
 const netflixController = {
