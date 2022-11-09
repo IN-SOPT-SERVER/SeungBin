49c5594c5785d44beeda4ee3e1b1004c11f4a9b6
csb9427 <csb9427@naver.com>
2022-11-09 15:42:35 +0900

    chore: index파일에 export 설정

--- seminar4/src/router/index.ts
+++ seminar4/src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import netflixRouter from "./netflixRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
+router.use("/netflix", netflixRouter);
 
 export default router;
